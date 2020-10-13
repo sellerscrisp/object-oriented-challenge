4931a9c77697b0a45cdc8abb2d40aaf0600572e8
Sellers Crisp <sellershcrisp@gmail.com>
2020-10-13 16:44:37 -0500

    finished challenges

--- app.js
+++ app.js
@@ -13,13 +13,35 @@ class Vehicle {
 }
 
 class Car extends Vehicle {
-  //...
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 4;
+  }
 }
 
 class Motorcycle extends Vehicle {
-  //...
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2;
+  }
+  revEngine() {
+    return 'VROOM!!!';
+  }
 }
 
 class Garage {
-  //...
+  constructor(capacity) {
+    this.vehicles = [];
+    this.capacity = capacity;
+  }
+  add(newVehicle) {
+    if (!(newVehicle instanceof Vehicle)) {
+      return 'Only vehicles are allowed ' + 'in here!';
+    }
+    if (this.vehicles.length >= this.capacity) {
+      return `Sorry, we're full.`;
+    }
+    this.vehicles.push(newVehicle);
+    return 'Vehicle added!';
+  }
 }
